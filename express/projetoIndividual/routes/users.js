ccbb54753224b94c84c6dc95cc927435fd26692b
Diogo Thompson <diogo.thompson@amaiseducacao.com.br>
2024-12-30 18:42:09 -0300

    Exercício de fixação express (http4) e definição de cookies e sessions

--- express/projetoIndividual/routes/users.js
+++ express/projetoIndividual/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('usuarios');
+  res.render('users');
 });
 
 module.exports = router;
